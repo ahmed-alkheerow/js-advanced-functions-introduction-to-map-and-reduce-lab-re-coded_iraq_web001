da28cd7abe7ef03c0a70efc6b7689711e145162b
Ahmed Al-kheerow <a.alkheerow@gmail.com>
2020-06-12 23:19:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,23 +6,35 @@ function mapToNegativize(sourceArray){
   )
 return newArry;
 };
+
 function mapToNoChange(sourceArray) {
   let newArry = [...sourceArray]
   return newArry;
-}
+};
+
 function mapToDouble(sourceArray) {
   let newArry=[]
   sourceArray.map(element => {
     newArry.push(element*2)
   })
   return newArry
+};
 
-}
 function mapToSquare(sourceArray) {
   let newArry=[]
   sourceArray.map(element => {
     newArry.push(element**2)
   })
-  return newArry
+  return newArry;
+};
 
+
+
+function reduceToTotal(sourceArray, startingPoint=0 ) {
+  let total =  startingPoint;
+  sourceArray.forEach(element =>{
+    total += element
+  })
+  return total;
+  
 }
